bda1818d38382c11fe4ee0f42f5545fd48f0c7d8
trimm <robertjessecooke@gmail.com>
2013-09-02 00:10:15 +0200

    upload init site

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var startDate   = "January 1, 2013 00:00:00";
+var endDate     = "January 1, 2014 00:00:00";
+var timeRefresh = 200;
+var dotTension  = 3.5;
+var dotDelay    = 1500;
+var marginSpan  = 250;
